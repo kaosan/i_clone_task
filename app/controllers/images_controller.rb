f563c4c5472b73bd867d0cc5484ee8cd45450ecb
Kaori <xiaxulizhaiteng@gmail.com>
2019-01-12 17:16:27 +0900

    init

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,5 +1,5 @@
 class ImagesController < ApplicationController
-  before_action :set_image, only: [:show, :edit, :update, :destro]
+  before_action :set_image, only: [:show, :edit, :update, :destroy]
 before_action :logged_in?, only: [:new, :edit, :show, :destroy]
 
   def index
@@ -67,13 +67,13 @@ before_action :logged_in?, only: [:new, :edit, :show, :destroy]
 
   def set_image
   @image = Image.find(params[:id])
-end
-
-def logged_in?
-  unless current_user.present?
-    flash[:notice] = 'ログインしてください'
-    redirect_to new_session_path
   end
-end
+
+  def logged_in?
+    unless current_user.present?
+      flash[:notice] = 'ログインしてください'
+      redirect_to new_session_path
+    end
+  end
 
 end
--- app/models/image.rb
+++ app/models/image.rb
@@ -1,6 +1,6 @@
 class Image < ApplicationRecord
   mount_uploader :image, ImageUploader
-  validates :title, presence: true
+  validates :content, presence: true
   validates :image, presence: true
   belongs_to :user
   has_many :favorites, dependent: :destroy

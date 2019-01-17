53c0d0d019bb2954a208343db11b4fe23e1ef1a7
Kaori <xiaxulizhaiteng@gmail.com>
2019-01-17 22:56:09 +0900

    init

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :logged_in?, only: [:show, :edit, :new, :destroy]
+  before_action :logged_in?, only: [:show, :edit, :update, :favorite]
   def new
     @user = User.new
   end
@@ -18,9 +18,23 @@ class UsersController < ApplicationController
     @favorites_images = @user.favorites
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to users_path, notice: "プロフィールを編集しました！"
+    else
+      render 'edit'
+    end
+  end
+
   private
 
   def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation, :icon, :icon_cache)
+    params.require(:user).permit(:name, :email, :password, :password_confirmation, :image, :image_cache)
   end
+
 end

34d50c2bb14fc5c188d9c0e31dd1d3e44adacb27
Kaori <xiaxulizhaiteng@gmail.com>
2019-01-18 13:16:40 +0900

    init

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -34,7 +34,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation, :image, :image_cache)
+    params.require(:user).permit(:name, :email, :password, :password_confirmation, :icon, :icon_cache)
   end
 
 end

633e50260cbe2d0be33892d9c2056896da6bd7d2
Kaori <xiaxulizhaiteng@gmail.com>
2019-01-27 16:36:03 +0900

    init

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -33,4 +33,6 @@ Rails.application.configure do
   end
 
   config.active_record.dump_schema_after_migration = false
+  config.action_mailer.default_url_options = { host: 'https://thawing-beyond-53735.herokuapp.com/'}
+ config.action_mailer.delivery_method = :letter_opener_web
 end
